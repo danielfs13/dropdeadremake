eed1a50388d9f4b8899a36e0cd99853588a003c8
Daniel Strano <88854271+danielfs13@users.noreply.github.com>
2021-08-29 22:24:34 -0300

    Add files via upload
    Dropdown menu das seções de roupa adicionado.
    Página de contato estilizada.

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+function myFunction() {
+  var x = document.getElementById("clotheslist");
+  if (x.className.indexOf("w3-show") == -1) { 
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
 const swiper = new Swiper('.swiper-container', {
 
   // And if we need scrollbar
